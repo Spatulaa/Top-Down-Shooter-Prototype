b64f1a8694f6d68087bcd9bf9c10f874d3c6e59b
Kevin Dang <kevindang2008@gmail.com>
2021-12-21 17:09:25 -0800

    some edits

--- gun.py
+++ gun.py
@@ -45,7 +45,7 @@ class Gun():
         #Spawning Bullet
         if pygame.time.get_ticks() - self.lastShot >= self.fireRate * 100:
             import main
-            main.CreateParticles(True, startPos, [6, 8], [2, 4], 10, [[255, 50, 0], [255, 255, 0]])
+            main.CreateParticles(True, startPos, [6, 8], [2, 3], 10, [[255, 50, 0], [255, 255, 0]])
             bullets.add(bullet.Bullet(startPos, shootDir, 6, False))
             self.lastShot = pygame.time.get_ticks()
         
